75762f849451c0295d2c3a0da44888befcc15492
KaNology <an.le091000@gmail.com>
2020-12-18 12:54:00 +0700

    add

--- client/src/components/Popup/PopupHandle.js
+++ client/src/components/Popup/PopupHandle.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Popup from './Popup.js';
+import './Popup.css'
+import {AuthorForm} from '../AuthorForm/AuthorForm'
  
 function PopupHandle() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,15 +12,14 @@ function PopupHandle() {
  
   return <div>
     <input
+      className = "add-button"
       type="button"
-      value="Click to Open Popup"
+      value="Add Book"
       onClick={togglePopup}
     />
     {isOpen && <Popup
       content={<>
-        <b>Design your Popup</b>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button>Test button</button>
+        <AuthorForm></AuthorForm>
       </>}
       handleClose={togglePopup}
     />}
